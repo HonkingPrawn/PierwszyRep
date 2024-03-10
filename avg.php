6efeafe0cd96c8203f3790ec7dc967e7bef03ab2
HonkingPrawn <j.kretczynski@gmail.com>
2024-03-10 10:57:18 +0100

    Funkcja srednia

---
+++ avg.php
@@ -0,0 +1,15 @@
+int a= 3;
+int b= 4;
+int dzielnik= 2;
+
+int suma= a+b;
+
+int srednia= suma/dzielnik;
+System.println(srednia);
+
+int c= 3;
+int b= 4;
+int dzielnik= 2;
+
+int suma= a+b;
+double waga= 9.0;
